d51b2cdd5f46880487c78a6d12801522c9edfdf6
Ning Wang <nw8333@princeton.edu>
2026-03-09 18:28:22 -0400

    What: Release v0.1.1 Why: Update public version to v0.1.1 How: Update version number and __main__.py run script Testing: No tests yet

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ with open('README.md', mode='r') as readme:
 
 setup(
     name='PIANO',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
         # Machine learning imports
