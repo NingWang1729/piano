e3d21cc78b08d68e0faebf575518eb538aca17bc
Ning Wang <nw8333@princeton.edu>
2026-03-16 13:46:54 -0400

    What: Update version number to 0.1.4 How: change version from 0.1.3 to 0.1.4 Why: Keep versioning consistent with pypi release Testing: testpypi environment works

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ with open('README.md', mode='r') as readme:
 
 setup(
     name='PIANO',
-    version='0.1.3',
+    version='0.1.4',
     packages=find_packages(),
     install_requires=[
         # Machine learning imports
