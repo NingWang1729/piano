d97daa6e39abb5a4f2b9fd64b6df7cb0babd1ebc
Ning Wang <nw8333@princeton.edu>
2026-04-21 18:11:41 -0400

    What: Update version to 0.1.7 How: Add the params for dist and parameterization to run_piano_integration.py Why: Make using the new options and bug fix for zinb easier Testing: Works for adv, nb vs zinb, mu-theta vs ksi-psi combinations

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ with open('README.md', mode='r') as readme:
 
 setup(
     name='PIANO',
-    version='0.1.6',
+    version='0.1.7',
     packages=find_packages(),
     install_requires=[
         # Machine learning imports
