f22642d65562b7f4230015b13504547ede789aac
Ning Wang <nw8333@scotty.pni.princeton.edu>
2026-04-25 19:02:45 -0400

    What: Update version to 0.1.8 Why: New bugfix and release of sparse continuous covariates How: Bump version from 0.1.7 to 0.1.8 Testing: No tests

--- piano/__init__.py
+++ piano/__init__.py
@@ -19,7 +19,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # piano/__init__.py
 
 # Define package version
-__version__ = '0.1.7'
+__version__ = '0.1.8'
 
 # Import all modules
 from .models.base_models import Etude, EtudeMuTheta
